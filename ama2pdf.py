e0b59fcba90eb5cfe6c12d3810d342cdb4c9c4c6
turkin501 <pham.viettai.12@gmail.com>
2023-05-18 16:16:09 +0700

    create index type selection

--- ama2pdf.py
+++ ama2pdf.py
@@ -12,6 +12,7 @@ from llama_index import GPTVectorStoreIndex, GPTListIndex, GPTKeywordTableIndex,
 from llama_index.indices.knowledge_graph.base import GPTKnowledgeGraphIndex
 from llama_index import load_index_from_storage
 from langchain import OpenAI
+from enum import Enum
 import PyPDF2
 # import gdown
 import os
@@ -31,6 +32,13 @@ def extract_pdf(file_path):
     text = "".join(list_text)
     return text
 
+def create_index_from_text(text, index_type):
+    index_types = [GPTVectorStoreIndex, GPTListIndex, GPTKeywordTableIndex, GPTKnowledgeGraphIndex, GPTTreeIndex]
+    methods = [create_vector_index_from_text, create_list_index_from_text, create_tree_index_from_text,
+               create_kwtable_index_from_text, create_kg_index_from_text]
+    n = index_types.index(index_type)
+    index = methods[n](text)
+    return index
 
 def create_vector_index_from_text(text):
     documents = [Document(text)]
@@ -62,9 +70,19 @@ def get_pdf_file():
 
     return save_path
 
-def save_indexes(index):
+def choose_index_type():
+    index_types = [GPTVectorStoreIndex, GPTListIndex, GPTKeywordTableIndex, GPTKnowledgeGraphIndex, GPTTreeIndex]
+    dirs = ["vector_index", "list_index", "kwtable_index", "graph_index", "tree_index"]
+    for i, index_type in enumerate(index_types):
+        print(i, ": ", index_type.__name__)
+    n = -1
+    while n not in range(len(dirs)):
+        n = int(input("Please choose the method you want to index: "))
+    return index_types[n], dirs[n]
+
+def save_indexes(index, dir):
     parent_dir = os.getcwd()
-    persist_dir = os.path.join(parent_dir, "index_dir")
+    persist_dir = os.path.join(parent_dir, dir)
 
     try:
         os.makedirs(persist_dir)
@@ -90,10 +108,12 @@ def main():
 
     pdf_file_path = get_pdf_file()
     text = extract_pdf(pdf_file_path)
-    index = create_list_index_from_text(text)
+
+    index_type, dir = choose_index_type()
+    index = create_index_from_text(text, index_type)
 
     # save indexes 
-    persist_dir = save_indexes(index)
+    persist_dir = save_indexes(index, dir)
 
     # load indexes
     storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
